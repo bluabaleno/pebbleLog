93f2458bdef8de696494a06db9263354d4920848
Jacky <balenodevelopment@gmail.com>
2015-10-16 00:49:52 -0400

    Added word objects

--- browser_action/browser_action.js
+++ browser_action/browser_action.js
@@ -1,12 +1,19 @@
-title = chrome.il8n.getMessages("extName");
+var wordsWritten = document.getElementsByTagName("var");
 
-<script>
+var words = [];
 
+for (var i = 0; i < wordsWritten.length; i++){
+	wordsWritten[i].addEventListener("click", function(){
+		var element = document.getElementById('v0');
+		var text = element.innerText;
+		var bold = document.getElementsByTagName("strong")[1].innerText;
+		var str = text.replace(bold,"");
+		var word;
+		var translation;
+		eval ("word" + "=" + "bold");
+		eval ("translation" + "=" + "str");
+		words.push(eval ({word,translation}));
 
-window.onload = function(){
-
-		console.log("Hello World");
-
-	};
+		console.log(words);
+	})
 };
-
