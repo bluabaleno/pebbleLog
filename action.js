bd5b611fe0ccf229a275d377e99a298dad701c86
Jacky <balenodevelopment@gmail.com>
2015-10-29 12:22:43 -0400

    adding Firebase data retrivel

--- action.js
+++ action.js
@@ -1,7 +1,10 @@
+
 var wordsWritten = document.getElementsByTagName("var");
 
 var wordBank ={};
+var fireBank = new Firebase('https://lernubaleno.firebaseio.com/');
 
+//Key-Value enclosed Object
 for (var i = 0; i < wordsWritten.length; i++){
 	wordsWritten[i].addEventListener("click", function(){
 		if (document.getElementsByClassName("vortarobobelo").length != 0){
@@ -12,8 +15,34 @@ for (var i = 0; i < wordsWritten.length; i++){
 				var word = words[i].children[0].innerText; // the word
 				var translation = words[i].children[1].innerText; // the translation
 				wordBank[word] = translation;
-				console.log(word,translation);
+				fireBank.update(wordBank);
 			}
 		}			
 	})
 };
+
+//Object enclosed array
+
+// var wordsWritten = document.getElementsByTagName("var");
+
+// var wordBank =[];
+// var fireBank = new Firebase('https://lernubaleno.firebaseio.com/');
+// for (var i = 0; i < wordsWritten.length; i++){
+// 	wordsWritten[i].addEventListener("click", function(){
+// 		if (document.getElementsByClassName("vortarobobelo").length != 0){
+// 			var wordbubble = document.getElementsByClassName("vortarobobelo")[0].children[0]
+// 			var words = wordbubble.children
+
+// 			for (var i = 0; i < words.length; i++ ){
+// 				var currentWord = words[i].children[0].innerText; // the word
+// 				var currentTranslation = words[i].children[1].innerText; // the translation
+// 				var wordTranslation = {
+// 					word: currentWord,
+// 					translation:currentTranslation
+// 					};
+// 				wordBank.push(wordTranslation);
+// 				fireBank.update(wordBank);
+// 			}
+// 		}			
+// 	})
+// };
--- browser_action/browser_action.js
+++ browser_action/browser_action.js
@@ -1,18 +1,47 @@
-var wordsWritten = document.getElementsByTagName("var");
+// test of loading the DOM before the script execute
+var app = angular.module("myApp",["firebase"]);
 
-var wordBank ={};
+app.controller("myAppCtrl", ["$scope", "$firebaseObject",
+          function($scope,$firebaseObject){
+          var ref = new Firebase("https://lernubaleno.firebaseio.com/");
+            console.log(ref);
+          $scope.data = $firebaseObject(ref);
+}]);
 
-for (var i = 0; i < wordsWritten.length; i++){
-	wordsWritten[i].addEventListener("click", function(){
-		if (document.getElementsByClassName("vortarobobelo").length != 0){
-			var wordbubble = document.getElementsByClassName("vortarobobelo")[0].children[0]
-			var words = wordbubble.children
-			for (var i = 0; i < words.length; i++ ){
-				var word = words[i].children[0].innerText; // the word
-				var translation = words[i].children[1].innerText; // the translation
-				wordBank[word] = translation;
-				console.log(word,translation);
-			}
-		}			
-	})
-};
+// var wordThere 
+
+// // fireBank.on("value", function(snapshot){
+// // 	console.log(snapshot.val());
+// // 	wordThere = snapshot.val();
+// // });
+
+// myApp.factory("steps",["$firebaseObject"])
+// myApp.controller('AppController', function ($scope) {
+//     $scope.steps;
+//     fireBank.on("value", function(snapshot){
+// 		$scope.steps = snapshot.val();
+// 		wordThere = snapshot.val();
+// 	});
+// });
+
+// document.addEventListener("DOMContentLoaded", function(){
+// 	document.getElementById("wordListHTML").innerHTML = "Hello there";
+// 	console.log("I loaded!");
+// });
+
+
+//getting the word and translation
+// for(var key in wordThere){
+// 	var val = wordThere[key];
+// 	console.log(key+ " " + val);
+// }
+
+//document.getElementById("wordListHTML").innerHTML 
+
+//gets the words back alphabetically
+// var wordHere = [];
+// fireBank.orderByKey().on("child_added", function(snapshot){
+// 	worhHere.push = snapshot.key() + ": " + snapshot.val();
+// 	console.log(snapshot.key() + ": " + snapshot.val());
+// 	document.getElementById("wordListHTML").innerHTML = snapshot.key() + " " + snapshot.val();
+// });
